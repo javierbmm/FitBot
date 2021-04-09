ee9951e3faba99bed224be86fa6f4c47ddffd735
Javier <javier.merida@mykukun.com>
2021-04-09 20:39:19 +0200

    npl file

---
+++ js/nlp.js
@@ -0,0 +1,14 @@
+/*
+tokenize();          -> Barbara (String) -> Array<String>
+//removeStopWords();   -> Barbara (Array<String>) -> Array<String> [optional]
+removePunctuation(); -> Nikki   (Array<String>) -> Array<String>
+stemmize();          -> Nikki   (Array<String>) -> Array<String>
+lemmatize();         -> Javier  (Array<String>) -> Array<String>
+*/
+
+/**
+ * return -
+ */
+function tokenize(){
+
+}
