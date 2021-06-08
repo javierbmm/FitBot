d32194004dfdf4e5944739d1dee3d9be61a10d59
Barbara <barbara.rekowska@magasin.dk>
2021-06-08 11:42:21 +0200

    Refactoring routines

--- data/routines.js
+++ data/routines.js
@@ -181,14 +181,206 @@ export const gain_strength = [
             "days": 4,
             "level": "intermediate"
         },
-        "routine": []
+        "routine": [
+            [
+                {
+                    "language": 2,
+                    "name": "Braced Squat"
+                },
+                {
+                    "language": 2,
+                    "name": "Bench Press"
+                },
+                {
+                    "language": 2,
+                    "name": "Deadlifts"
+                },
+                {
+                    "language": 2,
+                    "name": "Shoulder Fly"
+                }
+            ],
+            [
+                {
+                    "language": 2,
+                    "name": "Bench Press"
+                },
+                {
+                    "language": 2,
+                    "name": "Seated Triceps Press"
+                },
+                {
+                    "language": 2,
+                    "name": "Leg Press"
+                },
+                {
+                    "language": 2,
+                    "name": "Bent Over Barbell Row"
+                }
+            ],
+            [
+                {
+                    "language": 2,
+                    "name": "Deadlifts"
+                },
+                {
+                    "language": 2,
+                    "name": "Bench Press"
+                },
+                {
+                    "language": 2,
+                    "name": "Biceps Curls With Barbell"
+                },
+                {
+                    "language": 2,
+                    "name": "Shoulder Fly"
+                }
+            ],
+            [
+                {
+                    "language": 2,
+                    "name": "Arnold Shoulder Press"
+                },
+                {
+                    "language": 2,
+                    "name": "Chin-ups"
+                },
+                {
+                    "language": 2,
+                    "name": "Seated Triceps Press"
+                },
+                {
+                    "language": 2,
+                    "name": "Lateral Raises"
+                }
+            ]
+        ]
     },
     {
         "options": {
             "days": 5,
             "level": "advanced"
         },
-        "routine": []
+        "routine": [
+            [
+                {
+                    "language": 2,
+                    "name":"Bench Press"
+                },
+                {
+                    "language": 2,
+                    "name": "Cable Cross-over"
+                },
+                {
+                    "language": 2,
+                    "name": "Cross-Bench Dumbbell Pullovers"
+                },
+                {
+                    "language": 2,
+                    "name": "Decline Bench Press Barbell"
+                },
+                {
+                    "language": 2,
+                    "name": "Push Ups"
+                }
+            ],
+            [
+                {
+                    "language": 2,
+                    "name":"Back Squat"
+                },
+                {
+                    "language": 2,
+                    "name": "Leg Press"
+                },
+                {
+                    "language": 2,
+                    "name":"Leg Extension"
+                },
+                {
+                    "language": 2,
+                    "name":"Leg Curl"
+                },
+                {
+                    "language": 2,
+                    "name":"Calf Raises"
+                }
+            ],
+            [
+                {
+                    "language": 2,
+                    "name": "Deadlifts"
+                },
+                {
+                    "language": 2,
+                    "name": "Cable Cross-over"
+                },
+                {
+                    "language": 2,
+                    "name": "Close-grip Lat Pull Down"
+                },
+                {
+                    "language": 2,
+                    "name": "Bent Over Barbell Row"
+                },
+                {
+                    "language": 2,
+                    "name": "Hyperextensions"
+                },
+                {
+                    "language": 2,
+                    "name": "Pull-ups"
+                }
+            ],
+            [
+                {
+                    "language": 2,
+                    "name":"Biceps Curls With Barbell"
+                },
+                {
+                    "language": 2,
+                    "name": "Biceps Curls With Dumbbell"
+                },
+                {
+                    "language": 2,
+                    "name": "Dips"
+                },
+                {
+                    "language": 2,
+                    "name": "Dumbbell Triceps Extension"
+                },
+                {
+                    "language": 2,
+                    "name": "French Press (skullcrusher) Dumbbells"
+                }
+            ],
+            [
+                {
+                    "language": 2,
+                    "name":"Arnold Shoulder Press"
+                },
+                {
+                    "language": 2,
+                    "name":"Bent-over Lateral Raises"
+                },
+                {
+                    "language": 2,
+                    "name": "Hammercurls"
+                },
+                {
+                    "language": 2,
+                    "name": "Plank"
+                },
+                {
+                    "language": 2,
+                    "name":"Crunches"
+                },
+                {
+                    "language": 2,
+                    "name":"Crunches With Legs Up"
+                }
+            ]
+        ]
     }
 ];
 
